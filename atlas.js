25080584ca5b293e812854566dd292a38a380011
alexkempf <ackempf@eagles.usi.edu>
2016-06-22 13:53:28 -0500

    initial commit

---
+++ atlas.js
@@ -0,0 +1,179 @@
+﻿(function () {
+  'use strict';
+  angular
+    .module('atlas.angular', [])
+
+  angular
+    .module('atlas.angular.filters', [])
+    .directive('dateFilter', dateFilter)
+
+  // https://gist.github.com/rhutchison/c8c14946e88a1c8f9216
+  angular
+    .module('atlas.angular.showErrors', [])
+    .directive('showErrors', showErrors)
+
+  angular
+    .module('atlas.angular.navbar', [])
+    .controller('AtlasNavController', AtlasNavController)
+    .directive('atlasNav', atlasNav)
+    .directive('atlasCommandLeft', atlasCommandLeft)
+    .directive('atlasCommandCenter', atlasCommandCenter)
+    .directive('atlasCommandRight', atlasCommandRight)
+    .directive('atlasCommand', atlasCommand)
+
+  function dateFilter(filter) {
+    return {
+      require: 'ngModel',
+      link: function (scope, element, attrs, ngModel) {
+        ngModel.$parsers.push(function (viewValue) {
+          return filter(viewValue, 'MM/dd/yyyy');
+        });
+      }
+    };
+  }
+
+  showErrors.$inject = ['$timeout', '$interpolate'];
+  function showErrors($timeout, $interpolate) {
+    var directive = {
+      restrict: 'A',
+      require: '^form',
+      compile: compile
+    };
+
+    return directive;
+
+    function compile(elem, attrs) {
+      if (attrs.showErrors.indexOf('skipFormGroupCheck') === -1) {
+        if (!(elem.hasClass('form-group') || elem.hasClass('input-group'))) {
+          throw new Error('show-errors element does not have the \'form-group\' or \'input-group\' class');
+        }
+      }
+
+      return linkFn;
+
+      function linkFn(scope, el, attrs, formCtrl) {
+        var inputEl,
+          inputName,
+          inputNgEl,
+          options,
+          showSuccess,
+          initCheck = false,
+          showValidationMessages = false;
+
+        options = scope.$eval(attrs.showErrors) || {};
+        showSuccess = options.showSuccess || false;
+        inputEl = el[0].querySelector('.form-control[name]') || el[0].querySelector('[name]');
+        inputNgEl = angular.element(inputEl);
+        inputName = $interpolate(inputNgEl.attr('name') || '')(scope);
+
+        if (!inputName) {
+          throw new Error('show-errors element has no child input elements with a \'name\' attribute class');
+        }
+
+        scope.$watch(function () {
+          return formCtrl[inputName] && formCtrl[inputName].$invalid;
+        }, toggleClasses);
+
+        scope.$on('show-errors-check-validity', checkValidity);
+        scope.$on('show-errors-reset', reset);
+
+        function checkValidity(event, name) {
+          if (angular.isUndefined(name) || formCtrl.$name === name) {
+            initCheck = true;
+            showValidationMessages = true;
+
+            return toggleClasses(formCtrl[inputName].$invalid);
+          }
+        }
+
+        function reset(event, name) {
+          if (angular.isUndefined(name) || formCtrl.$name === name) {
+            return $timeout(function () {
+              el.removeClass('has-error');
+              el.removeClass('has-success');
+              showValidationMessages = false;
+            }, 0, false);
+          }
+        }
+
+        function toggleClasses(invalid) {
+          el.toggleClass('has-error', showValidationMessages && invalid);
+
+          if (showSuccess) {
+            return el.toggleClass('has-success', showValidationMessages && !invalid);
+          }
+        }
+      }
+    }
+  }
+
+  function atlasNav() {
+    return {
+      restrict: 'E',
+      templateUrl: 'template/navigation/navbar.html',
+      transclude: true,
+      scope: {
+        username: '=username',
+        showCommand: '@showCommand',
+      },
+      controller: 'AtlasNavController'
+    };
+  }
+
+  function atlasCommandLeft() {
+    return {
+      restrict: 'E',
+      templateUrl: 'template/navigation/command-left.html',
+      transclude: true
+    };
+  }
+
+  function atlasCommandCenter() {
+    return {
+      restrict: 'E',
+      templateUrl: 'template/navigation/command-center.html',
+      transclude: true
+    };
+  }
+
+  function atlasCommandRight() {
+    return {
+      restrict: 'E',
+      templateUrl: 'template/navigation/command-right.html',
+      transclude: true
+    };
+  }
+
+  function atlasCommand() {
+    return {
+      restrict: 'E',
+      templateUrl: 'template/navigation/command.html',
+      scope: {
+        title: '@title',
+        text: '@text',
+        icon: '@icon',
+        clickEvent: '&clickEvent'
+      },
+      link: function ($scope, elem, attrs) {
+        $scope.title = attrs.title;
+        $scope.text = attrs.text;
+        $scope.icon = attrs.icon;
+      }
+    };
+  }
+
+  AtlasNavController.$inject = ['$scope', '$rootScope'];
+  function AtlasNavController($scope, $rootScope) {
+    var isShown = $scope.showCommand === 'true'
+    if (isShown) {
+      $rootScope.bodyCss = 'body-regular';
+    }
+    else {
+      $rootScope.bodyCss = 'body-short';
+    }
+    $scope.isMainCollapsed = true;
+    $scope.isCommandCollapsed = true;
+  }
+
+
+})();
---
+++ templates/atlas.tpls.js
@@ -0,0 +1,271 @@
+﻿/**
+  * Day Picker Template Override
+  */
+angular.module('uib/template/datepicker/day.html', []).run(['$templateCache', function ($templateCache) {
+  'use strict';
+
+  $templateCache.put('uib/template/datepicker/day.html',
+    "<table class=\"uib-daypicker\" role=\"grid\" aria-labelledby=\"{{::uniqueId}}-title\" aria-activedescendant=\"{{activeDateId}}\">\n" +
+    "  <thead>\n" +
+    "    <tr>\n" +
+    "      <th><button type=\"button\" class=\"btn btn-default pull-left uib-left\" ng-click=\"move(-1)\" tabindex=\"-1\"><i class=\"fa fa-chevron-left\"></i></button></th>\n" +
+    "      <th colspan=\"{{::5 + showWeeks}}\"><button id=\"{{::uniqueId}}-title\" role=\"heading\" aria-live=\"assertive\" aria-atomic=\"true\" type=\"button\" class=\"btn btn-default uib-title\" ng-click=\"toggleMode()\" ng-disabled=\"datepickerMode === maxMode\" tabindex=\"-1\"><strong>{{title}}</strong></button></th>\n" +
+    "      <th><button type=\"button\" class=\"btn btn-default pull-right uib-right\" ng-click=\"move(1)\" tabindex=\"-1\"><i class=\"fa fa-chevron-right\"></i></button></th>\n" +
+    "    </tr>\n" +
+    "    <tr >\n" +
+    "      <th ng-if=\"showWeeks\" class=\"text-center\"></th>\n" +
+    "      <th ng-repeat=\"label in ::labels track by $index\" class=\"text-center\"><small aria-label=\"{{::label.full}}\">{{::label.abbr.substring(0,2)}}</small></th>\n" +
+    "    </tr>\n" +
+    "  </thead>\n" +
+    "  <tbody>\n" +
+    "    <tr class=\"uib-weeks\" ng-repeat=\"row in rows track by $index\">\n" +
+    "      <td ng-if=\"showWeeks\" class=\"text-center h6\"><em>{{ weekNumbers[$index] }}</em></td>\n" +
+    "      <td ng-repeat=\"dt in row\" class=\"uib-day text-center\" role=\"gridcell\"\n" +
+    "        id=\"{{::dt.uid}}\"\n" +
+    "        ng-class=\"::dt.customClass\">\n" +
+    "        <button type=\"button\" class=\"btn btn-default\"\n" +
+    "          uib-is-class=\"\n" +
+    "            'btn-primary' for selectedDt\n" +
+    "            on dt\"\n" +
+    "          ng-click=\"select(dt.date)\"\n" +
+    "          ng-disabled=\"::dt.disabled\"\n" +
+    "          tabindex=\"-1\"><span ng-class=\"::{'text-muted': dt.secondary}\">{{::dt.label}}</span></button>\n" +
+    "      </td>\n" +
+    "    </tr>\n" +
+    "  </tbody>\n" +
+    "</table>\n" +
+    "");
+}]);
+
+/**
+  * Month Picker Template Override
+  */
+angular.module('uib/template/datepicker/month.html', []).run(['$templateCache', function ($templateCache) {
+  $templateCache.put('uib/template/datepicker/month.html',
+    "<table class=\"uib-monthpicker\" role=\"grid\" aria-labelledby=\"{{::uniqueId}}-title\" aria-activedescendant=\"{{activeDateId}}\">\n" +
+    "  <thead>\n" +
+    "    <tr>\n" +
+    "      <th><button type=\"button\" class=\"btn btn-default pull-left uib-left\" ng-click=\"move(-1)\" tabindex=\"-1\"><i class=\"fa fa-chevron-left\"></i></button></th>\n" +
+    "      <th><button id=\"{{::uniqueId}}-title\" role=\"heading\" aria-live=\"assertive\" aria-atomic=\"true\" type=\"button\" class=\"btn btn-default uib-title\" ng-click=\"toggleMode()\" ng-disabled=\"datepickerMode === maxMode\" tabindex=\"-1\"><strong>{{title}}</strong></button></th>\n" +
+    "      <th><button type=\"button\" class=\"btn btn-default pull-right uib-right\" ng-click=\"move(1)\" tabindex=\"-1\"><i class=\"fa fa-chevron-right\"></i></button></th>\n" +
+    "    </tr>\n" +
+    "  </thead>\n" +
+    "  <tbody>\n" +
+    "    <tr class=\"uib-months\" ng-repeat=\"row in rows track by $index\">\n" +
+    "      <td ng-repeat=\"dt in row\" class=\"uib-month text-center\" role=\"gridcell\"\n" +
+    "        id=\"{{::dt.uid}}\"\n" +
+    "        ng-class=\"::dt.customClass\">\n" +
+    "        <button type=\"button\" class=\"btn btn-default\"\n" +
+    "          uib-is-class=\"\n" +
+    "            'btn-primary' for selectedDt\n" +
+    "            on dt\"\n" +
+    "          ng-click=\"select(dt.date)\"\n" +
+    "          ng-disabled=\"::dt.disabled\"\n" +
+    "          tabindex=\"-1\"><span>{{::dt.label}}</span></button>\n" +
+    "      </td>\n" +
+    "    </tr>\n" +
+    "  </tbody>\n" +
+    "</table>\n" +
+    "");
+}]);
+
+/**
+  * Year Picker Template Override
+  */
+angular.module('uib/template/datepicker/year.html', []).run(['$templateCache', function ($templateCache) {
+  $templateCache.put('uib/template/datepicker/year.html',
+    "<table class=\"uib-yearpicker\" role=\"grid\" aria-labelledby=\"{{::uniqueId}}-title\" aria-activedescendant=\"{{activeDateId}}\">\n" +
+    "  <thead>\n" +
+    "    <tr>\n" +
+    "      <th><button type=\"button\" class=\"btn btn-default pull-left uib-left\" ng-click=\"move(-1)\" tabindex=\"-1\"><i class=\"fa fa-chevron-left\"></i></button></th>\n" +
+    "      <th colspan=\"{{::columns - 2}}\"><button id=\"{{::uniqueId}}-title\" role=\"heading\" aria-live=\"assertive\" aria-atomic=\"true\" type=\"button\" class=\"btn btn-default uib-title\" ng-click=\"toggleMode()\" ng-disabled=\"datepickerMode === maxMode\" tabindex=\"-1\"><strong>{{title}}</strong></button></th>\n" +
+    "      <th><button type=\"button\" class=\"btn btn-default pull-right uib-right\" ng-click=\"move(1)\" tabindex=\"-1\"><i class=\"fa fa-chevron-right\"></i></button></th>\n" +
+    "    </tr>\n" +
+    "  </thead>\n" +
+    "  <tbody>\n" +
+    "    <tr class=\"uib-years\" ng-repeat=\"row in rows track by $index\">\n" +
+    "      <td ng-repeat=\"dt in row\" class=\"uib-year text-center\" role=\"gridcell\"\n" +
+    "        id=\"{{::dt.uid}}\"\n" +
+    "        ng-class=\"::dt.customClass\">\n" +
+    "        <button type=\"button\" class=\"btn btn-default\"\n" +
+    "          uib-is-class=\"\n" +
+    "            'btn-primary' for selectedDt\n" +
+    "            on dt\"\n" +
+    "          ng-click=\"select(dt.date)\"\n" +
+    "          ng-disabled=\"::dt.disabled\"\n" +
+    "          tabindex=\"-1\"><span>{{::dt.label}}</span></button>\n" +
+    "      </td>\n" +
+    "    </tr>\n" +
+    "  </tbody>\n" +
+    "</table>\n" +
+    "");
+}]);
+
+/**
+  * Time Picker Template Override
+  */
+angular.module('uib/template/timepicker/timepicker.html', []).run(['$templateCache', function ($templateCache) {
+  $templateCache.put('uib/template/timepicker/timepicker.html',
+    "<table class=\"uib-timepicker\">\n" +
+    "  <tbody>\n" +
+    "    <tr class=\"text-center\" ng-show=\"::showSpinners\">\n" +
+    "      <td class=\"uib-increment hours\"><a ng-click=\"incrementHours()\" ng-class=\"{disabled: noIncrementHours()}\" class=\"btn btn-link\" ng-disabled=\"noIncrementHours()\" tabindex=\"{{::tabindex}}\"><span class=\"glyphicon glyphicon-chevron-up\"></span></a></td>\n" +
+    "      <td>&nbsp;</td>\n" +
+    "      <td class=\"uib-increment minutes\"><a ng-click=\"incrementMinutes()\" ng-class=\"{disabled: noIncrementMinutes()}\" class=\"btn btn-link\" ng-disabled=\"noIncrementMinutes()\" tabindex=\"{{::tabindex}}\"><span class=\"glyphicon glyphicon-chevron-up\"></span></a></td>\n" +
+    "      <td ng-show=\"showSeconds\">&nbsp;</td>\n" +
+    "      <td ng-show=\"showSeconds\" class=\"uib-increment seconds\"><a ng-click=\"incrementSeconds()\" ng-class=\"{disabled: noIncrementSeconds()}\" class=\"btn btn-link\" ng-disabled=\"noIncrementSeconds()\" tabindex=\"{{::tabindex}}\"><span class=\"glyphicon glyphicon-chevron-up\"></span></a></td>\n" +
+    "      <td ng-show=\"showMeridian\"></td>\n" +
+    "    </tr>\n" +
+    "    <tr>\n" +
+    "      <td class=\"form-group uib-time hours\" ng-class=\"{'has-error': invalidHours}\">\n" +
+    "        <input type=\"text\" placeholder=\"HH\" ng-model=\"hours\" ng-change=\"updateHours()\" class=\"form-control text-center\" ng-readonly=\"::readonlyInput\" maxlength=\"2\" tabindex=\"{{::tabindex}}\" ng-disabled=\"noIncrementHours()\" ng-blur=\"blur()\">\n" +
+    "      </td>\n" +
+    "      <td class=\"uib-separator\">:</td>\n" +
+    "      <td class=\"form-group uib-time minutes\" ng-class=\"{'has-error': invalidMinutes}\">\n" +
+    "        <input type=\"text\" placeholder=\"MM\" ng-model=\"minutes\" ng-change=\"updateMinutes()\" class=\"form-control text-center\" ng-readonly=\"::readonlyInput\" maxlength=\"2\" tabindex=\"{{::tabindex}}\" ng-disabled=\"noIncrementMinutes()\" ng-blur=\"blur()\">\n" +
+    "      </td>\n" +
+    "      <td ng-show=\"showSeconds\" class=\"uib-separator\">:</td>\n" +
+    "      <td class=\"form-group uib-time seconds\" ng-class=\"{'has-error': invalidSeconds}\" ng-show=\"showSeconds\">\n" +
+    "        <input type=\"text\" placeholder=\"SS\" ng-model=\"seconds\" ng-change=\"updateSeconds()\" class=\"form-control text-center\" ng-readonly=\"readonlyInput\" maxlength=\"2\" tabindex=\"{{::tabindex}}\" ng-disabled=\"noIncrementSeconds()\" ng-blur=\"blur()\">\n" +
+    "      </td>\n" +
+    "      <td ng-show=\"showMeridian\" class=\"uib-time am-pm\"><button type=\"button\" ng-class=\"{disabled: noToggleMeridian()}\" class=\"btn btn-default text-center\" ng-click=\"toggleMeridian()\" ng-disabled=\"noToggleMeridian()\" tabindex=\"{{::tabindex}}\">{{meridian}}</button></td>\n" +
+    "    </tr>\n" +
+    "    <tr class=\"text-center\" ng-show=\"::showSpinners\">\n" +
+    "      <td class=\"uib-decrement hours\"><a ng-click=\"decrementHours()\" ng-class=\"{disabled: noDecrementHours()}\" class=\"btn btn-link\" ng-disabled=\"noDecrementHours()\" tabindex=\"{{::tabindex}}\"><span class=\"glyphicon glyphicon-chevron-down\"></span></a></td>\n" +
+    "      <td>&nbsp;</td>\n" +
+    "      <td class=\"uib-decrement minutes\"><a ng-click=\"decrementMinutes()\" ng-class=\"{disabled: noDecrementMinutes()}\" class=\"btn btn-link\" ng-disabled=\"noDecrementMinutes()\" tabindex=\"{{::tabindex}}\"><span class=\"glyphicon glyphicon-chevron-down\"></span></a></td>\n" +
+    "      <td ng-show=\"showSeconds\">&nbsp;</td>\n" +
+    "      <td ng-show=\"showSeconds\" class=\"uib-decrement seconds\"><a ng-click=\"decrementSeconds()\" ng-class=\"{disabled: noDecrementSeconds()}\" class=\"btn btn-link\" ng-disabled=\"noDecrementSeconds()\" tabindex=\"{{::tabindex}}\"><span class=\"glyphicon glyphicon-chevron-down\"></span></a></td>\n" +
+    "      <td ng-show=\"showMeridian\"></td>\n" +
+    "    </tr>\n" +
+    "  </tbody>\n" +
+    "</table>\n" +
+    "");
+}]);
+
+/**
+  * Pagination Template
+  */
+angular.module('atlas.angular').run(['$templateCache', function ($templateCache) {
+  'use strict';
+  $templateCache.put('template/pagination/pagination.html',
+    "<aside class=\"pager-right\">\n" +
+    "  <ul class=\"pagination\" ng-if=\"1 < pages.length || !autoHide\">\n" +
+    "    <li ng-if=\"boundaryLinks\" ng-class=\"{ disabled : pagination.current == 1 }\" title=\"First Page\">\n" +
+    "      <a href=\"\" ng-click=\"setCurrent(1)\">&laquo;</a>\n" +
+    "    </li>\n" +
+    "    <li ng-if=\"directionLinks\" ng-class=\"{ disabled : pagination.current == 1 }\" title=\"Previous Page\">\n" +
+    "      <a href=\"\" ng-click=\"setCurrent(pagination.current - 1)\">&lsaquo;</a>\n" +
+    "    </li>\n" +
+    "    <li ng-repeat=\"pageNumber in pages track by tracker(pageNumber, $index)\" ng-class=\"{ active : pagination.current == pageNumber, disabled : pageNumber == '...' }\">\n" +
+    "      <a href=\"\" ng-click=\"setCurrent(pageNumber)\">{{ pageNumber }}</a>\n" +
+    "    </li>\n" +
+    "    <li ng-if=\"directionLinks\" ng-class=\"{ disabled : pagination.current == pagination.last }\" title=\"Next Page\">\n" +
+    "      <a href=\"\" ng-click=\"setCurrent(pagination.current + 1)\">&rsaquo;</a>\n" +
+    "    </li>\n" +
+    "    <li ng-if=\"boundaryLinks\" ng-class=\"{ disabled : pagination.current == pagination.last }\" title=\"Last Page\">\n" +
+    "      <a href=\"\" ng-click=\"setCurrent(pagination.last)\">&raquo;</a>\n" +
+    "    </li>\n" +
+    "  </ul>\n" +
+    "</aside>\n" +
+    "<aside class=\"pager-count\">\n" +
+    "  <p>Displaying {{ range.lower }} - {{ range.upper }} of {{ range.total }} records</p>\n" +
+    "</aside>\n"
+    );
+}]);
+
+/**
+  * Navbar Template
+  */
+angular.module('atlas.angular.navbar').run(['$templateCache', function ($templateCache) {
+  'use strict';
+  $templateCache.put('template/navigation/navbar.html',
+    "  <nav class=\"navbar navbar-static-top navbar-short navbar-inverse\">\n" +
+    "    <div class=\"container\">\n" +
+    "      <div class=\"navbar-header\">\n" +
+    "        <button type=\"button\" class=\"navbar-toggle collapsed\" ng-click=\"isMainCollapsed = !isMainCollapsed\">\n" +
+    "          <span class=\"sr-only\">Toggle navigation</span>\n" +
+    "          <span class=\"fa fa-user\" style=\"color: white;\"></span>\n" +
+    "        </button>\n" +
+    "        <a class=\"navbar-brand\">\n" +
+    "          <div class=\"navbar-brand-name\">\n" +
+    "            <img class=\"brand-img\" src=\"/app/images/atlasnet-white.png\" />\n" +
+    "            Change Tracking\n" +
+    "          </div>\n" +
+    "        </a>\n" +
+    "      </div>\n" +
+    "      <div class=\"navbar-collapse collapse\" uib-collapse=\"isMainCollapsed\">\n" +
+    "        <ul class=\"nav navbar-nav navbar-right\">\n" +
+    "          <li>\n" +
+    "            <a href=\"\">{{username}}\n" +
+    "            </a>\n" +
+    "          </li>\n" +
+    "        </ul>\n" +
+    "      </div>\n" +
+    "    </div>\n" +
+    "  </nav>\n" +
+    "  <nav class=\"navbar navbar-static-top navbar-default\" ng-show=\"{{showCommand}}\">\n" +
+    "    <div class=\"container\">\n" +
+    "     <div class=\"navbar-header\">\n" +
+    "       <button type=\"button\" class=\"navbar-toggle collapsed\" ng-click=\"isCommandCollapsed = !isCommandCollapsed\">\n" +
+    "         <span class=\"sr-only\">Toggle navigation</span>\n" +
+    "         <span class=\"icon-bar\"></span>\n" +
+    "         <span class=\"icon-bar\"></span>\n" +
+    "         <span class=\"icon-bar\"></span>\n" +
+    "       </button>\n" +
+    "       </div>\n" +
+    "       <div class=\"navbar-collapse collapse\" uib-collapse=\"isCommandCollapsed\">\n" +
+    "          <div ng-transclude></div>\n" +
+    "       </div>\n" +
+    "     </div>\n" +
+    "   </nav>\n"
+    );
+}]);
+
+/**
+  * Command Center Template
+  */
+angular.module('atlas.angular.navbar').run(['$templateCache', function ($templateCache) {
+  'use strict';
+  $templateCache.put('template/navigation/command-center.html',
+    "<ul class=\"nav navbar-nav navbar-center\">\n" +
+    "  <div ng-transclude></div>\n" +
+    "</ul>\n"
+    );
+}]);
+/**
+  * Command Left Template
+  */
+angular.module('atlas.angular.navbar').run(['$templateCache', function ($templateCache) {
+  'use strict';
+  $templateCache.put('template/navigation/command-left.html',
+    "<ul class=\"nav navbar-nav\">\n" +
+    "  <div ng-transclude></div>\n" +
+    "</ul>\n"
+    );
+}]);
+/**
+  * Command Right Template
+  */
+angular.module('atlas.angular.navbar').run(['$templateCache', function ($templateCache) {
+  'use strict';
+  $templateCache.put('template/navigation/command-right.html',
+    "<ul class=\"nav navbar-nav navbar-right\">\n" +
+    "  <div ng-transclude></div>\n" +
+    "</ul>\n"
+    );
+}]);
+
+/**
+  * Command Template
+  */
+angular.module('atlas.angular.navbar').run(['$templateCache', function ($templateCache) {
+  'use strict';
+  $templateCache.put('template/navigation/command.html',
+    "<li tooltip-placement=\"bottom\" uib-tooltip=\"{{title}}\">\n" +
+    "  <a href=\"\" ng-click=\"clickEvent()\">\n" +
+    "    <i class=\"fa fa-{{icon}}\"></i> {{text}}\n" + 
+    "  </a>\n" +
+    "</li>\n"
+    );
+}]);
